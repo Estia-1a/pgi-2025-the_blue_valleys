ba2c280c376df15a4e048af7a48a45dd0ed445ab
tristanlaporte <tristan.laporte@etu.estia.fr>
2025-05-28 18:42:38 +0200

    Modification of tenth_pixel feature
    Replace 11 by 12 (number of tenth_pixels characters)

--- src/main.c
+++ src/main.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
       first_pixel(configuration.filenames[0]);}
     
-  if ( strncmp( configuration.command, "tenth_pixel", 12 ) == 0 ) {
+  if ( strncmp( configuration.command, "tenth_pixel", 11 ) == 0 ) {
     tenth_pixel(configuration.filenames[0]);}
   
   return 0;
