5a406ef73d0a32efd76ace418ed06c5482ca0b5d
tristanlaporte <tristan.laporte@etu.estia.fr>
2025-06-10 11:37:15 +0200

    add feature max component
    TristanL

--- src/features.c
+++ src/features.c
@@ -122,8 +122,45 @@ void min_pixel(char *source_path) {
     printf("min_pixel (%d,%d): %d, %d, %d", x_min, y_min, r_min, g_min, b_min);
 }
 
+void max_component(char *source_path, char component) {
+    if (component != 'R' && component != 'G' && component != 'B') {
+        printf("Le parametre de la fonction doit etre R, G ou B.");
+        return;
+    }
+    unsigned char *data;
+    int width, height, channel_count;
+    int i = 0;
+    unsigned char comp, comp_max;
+    int x_max, y_max;
+    read_image_data(source_path, &data, &width, &height, &channel_count);
+    int nb_pixels = width * height;
+    for (i=0; i<nb_pixels; i++) {
 
+        if (component == 'R') {
+            comp = data[i*3+0];
+            if (comp>comp_max) {
+                comp_max=comp;
+                x_max = i % width;
+                y_max = i / width;
+            }
+        }
 
-
-
-
+        else if (component == 'G') {
+            comp = data[i*3+1];
+            if (comp>comp_max) {
+                comp_max=comp;
+                x_max = i % width;
+                y_max = i / width;
+            }
+        }
+        else {
+            comp = data[i*3+2];
+            if (comp>comp_max) {
+                comp_max=comp;
+                x_max = i % width;
+                y_max = i / width;
+            }
+        }  
+    }
+    printf("max component %c (%d, %d): %d", component, x_max, y_max, comp_max);
+}
